ae0c71626c73cc1ed6d8735fca9aedc304802190
zimy <zimyx@outlook.com>
2018-02-24 13:22:26 +0800

    first commit

---
+++ assets/rmlink.js
@@ -0,0 +1,8 @@
+require([
+    'gitbook',
+    'jquery'
+],function(gitbook,$){
+    var link = $('.gitbook-link')
+    link.attr('href','http://zbcc.net.cn')
+    link.text('Published by Zimy')
+})
